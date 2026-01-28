86e6bd96656dbb5506d08e4f7ea169d8a6c01d3d
Jonathan020988 <jonathanramirez253631@correo.itm.edu.co>
2026-01-28 10:46:47 -0500

    login funcional con BCrypt y endpoint auth/login"

---
+++ WorldCup.Api/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorldCup.Api.Data;
+using WorldCup.Api.DTOs;
+
+using DocumentFormat.OpenXml.Math;
+
+namespace WorldCup.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO dto)
+        {
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(u => u.Email == dto.Email);
+
+            if (usuario == null)
+                return Unauthorized("Credenciales inválidas");
+
+            var passwordValida = BCrypt.Net.BCrypt.Verify(dto.Password, usuario.PasswordHash);
+
+            if (!passwordValida)
+                return Unauthorized("Credenciales inválidas");
+
+            return Ok(new UsuarioDTO
+            {
+                Id = usuario.Id,
+                Nombre = usuario.Nombre,
+                Email = usuario.Email
+            });
+        }
+    }
+}
