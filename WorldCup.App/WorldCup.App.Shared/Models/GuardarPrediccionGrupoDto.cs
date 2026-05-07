3b975eacb101d96216fcd5da05ec54577dd99de8
Jonathan020988 <181796257+Jonathan020988@users.noreply.github.com>
2026-05-07 17:46:13 -0500

    todo funcional, predicciones y clasificacion

--- WorldCup.App/WorldCup.App.Shared/Models/GuardarPrediccionGrupoDto.cs
+++ WorldCup.App/WorldCup.App.Shared/Models/GuardarPrediccionGrupoDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorldCup.App.Shared.DTOs;
+﻿namespace WorldCup.App.Shared.DTOs;
 
 public class GuardarPrediccionGrupoDto
 {
